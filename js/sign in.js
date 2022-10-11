ab969364c156b23918be823162af771cb675ad7a
chbani <chbani@gmail.com>
2022-10-11 02:10:36 +0100

    sign in the message error

--- js/sign in.js
+++ js/sign in.js
@@ -8,6 +8,18 @@ $(document).ready(function () {
   const eye = $(".eye");
   const btn = $(".btnn");
 
+
+  var jsonn = "ggg"; 
+  
+  fetch('./json/MessageEnglish.json')
+  .then(response => response.json())
+  .then(json => {
+    // console.log(json['valid username']);
+    jsonn = json['valid username'];
+  }).call(this);
+
+  console.log(jsonn);
+
   //start click en label focus to input
   labelsign.each((e) => {
     labelsign.eq(e).click((ele) => {
@@ -26,19 +38,20 @@ $(document).ready(function () {
         labelsign.eq(e).removeClass("top");
       }
       // end animate label
+ 
+      // start show and hed eye 
+      if (login__input[1].value.length >= 1 && eye.attr('class') == "eye" && login__input.eq(1).attr("type") == "password") {
+        eye.addClass("icon-eye-blocked");
+      }else if(login__input[1].value.length >= 1 && eye.attr('class') == "eye" && login__input.eq(1).attr("type") == "text") {
+        eye.addClass("icon-eye");
+      }else if (login__input[1].value.length < 1) {
+        eye.removeClass("icon-eye-blocked");
+        eye.removeClass("icon-eye");
+      };
+      // end show and hed eye 
+
     });  
   });
-
-  // start show and hed eye 
-  login__input[1].addEventListener("keyup", e => {
-    if (login__input[1].value.length >= 1 && eye.attr('class') == "eye") {
-      eye.addClass("icon-eye-blocked");
-    } if (login__input[1].value.length < 1) {
-      eye.removeClass("icon-eye-blocked");
-      eye.removeClass("icon-eye");
-    };
-  });
-  // end show and hed eye 
 
   //start click on eye 
   eye.click(ee => {
@@ -54,71 +67,62 @@ $(document).ready(function () {
     };
   });
   //end click on eye 
+  
 
 
   //start input blur
-  login__input[0].addEventListener("blur", (e) => {
-    if (!login__input[0].value.match(validation_email)) {
-      login__input.eq(0).css('border-bottom', '2px solid rgb(245 1 57)');
-    } else {
-      login__input.eq(0).css('border-bottom', '2px solid rgb(10 243 6)');
-    } 
-  });
-
-  login__input[1].addEventListener("blur", (e) => {
-    if (login__input[1].value == "") {
-      login__input.eq(1).css('border-bottom', '2px solid rgb(245 1 57)');
-    } else {
-      login__input.eq(1).css('border-bottom', '2px solid rgb(10 243 6)');
-    } 
+  login__input.each((e) => {
+    login__input[e].addEventListener("blur", (el) => {
+      if (login__input[e].value == "") {
+        login__input.eq(e).css('border-bottom', '2px solid rgb(245 1 57)');
+      } else {
+        login__input.eq(e).css('border-bottom', '2px solid rgb(10 243 6)');
+      } 
+    });
   });
   //end input blur
 
   //start btn click
   btn[0].addEventListener("click", (e) => {
-    e.preventDefault();
-    if (login__input[0].value.length == 0 && login__input[1].value.length == 0) {
+    if (login__input[0].value.length == 0) {
+      e.preventDefault();
       Swal.fire({
         icon: 'error',
-        text: 'please enter a valid email address and password',
+        text: "please enter a valid username",
       });
       login__input.eq(0).css('border-bottom', '2px solid rgb(245 1 57)');
-      login__input.eq(1).css('border-bottom', '2px solid rgb(245 1 57)');
-      e.preventDefault();
-    } else if (login__input[0].value.length == 0 && login__input[1].value.length > 0) {
-      Swal.fire({
-        icon: 'error',
-        text: 'please enter a valid email address',
-      });
-      login__input.eq(0).css('border-bottom', '2px solid rgb(245 1 57)');
-      e.preventDefault();
-    } else if (login__input[0].value.length > 0 && login__input[1].value.length == 0) {
-      Swal.fire({
-        icon: 'error',
-        text: 'please enter a valid password',
-      });
-      login__input.eq(1).css('border-bottom', '2px solid rgb(245 1 57)');
-      e.preventDefault();
-    } else {
-      if (!login__input[0].value.match(validation_email)) {
-        Swal.fire({
-          icon: 'error',
-          title: 'Email error',
-          text: 'example: users@gmail.com',
-        });
-      }else{
-        Swal.fire({
-          position: 'top-end',
-          icon: 'success',
-          title: 'Your work has been saved',
-          showConfirmButton: false,
-          timer: 1500
-        })
-        setTimeout(()=>{window.location.href='https://getbootstrap.com'},3000)
-        
-        
-      }
-    };
+    }  
+    // } else if (login__input[0].value.length == 0 && login__input[1].value.length > 0) {
+    //   Swal.fire({
+    //     icon: 'error',
+    //     text: 'please enter a valid email address',
+    //   });
+    //   login__input.eq(0).css('border-bottom', '2px solid rgb(245 1 57)');
+    //   e.preventDefault();
+    // } else if (login__input[0].value.length > 0 && login__input[1].value.length == 0) {
+    //   Swal.fire({
+    //     icon: 'error',
+    //     text: 'please enter a valid password',
+    //   });
+    //   login__input.eq(1).css('border-bottom', '2px solid rgb(245 1 57)');
+    //   e.preventDefault();
+    // } else {
+    //   if (!login__input[0].value.match(validation_email)) {
+    //     Swal.fire({
+    //       icon: 'error',
+    //       title: 'Email error',
+    //       text: 'example: users@gmail.com',
+    //     });
+    //   }else{
+    //     Swal.fire({
+    //       position: 'top-end',
+    //       icon: 'success',
+    //       title: 'Your work has been saved',
+    //       showConfirmButton: false,
+    //       timer: 1500
+    //     })
+    //   }
+    // };
     //end btn click
   });
   // -------------
